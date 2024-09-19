1bcd2346b38853c6474c2709f6c693b10dbabeeb
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-09-20 01:03:49 +0530

    Navbar commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Card from './components/Card/Card'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () =>{
+    return(
+        <>
+        <div className="header">
+            <h2 className="heading-1">PokeCard</h2>
+
+            <div className="search-container">
+                <input className="input" placeholder="Enter Name"/>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Navbar
