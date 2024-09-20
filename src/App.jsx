9abcaa52e1ff56d4af645667c900a8210453936f
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-09-20 19:46:02 +0530

    search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,52 @@
-import { useState,useEffect } from 'react'
-
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import Card from './components/Card/Card'
-import axios from 'axios'
-import { useModal } from './context/modal-context'
+import { useState, useEffect } from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Card from './components/Card/Card';
+import axios from 'axios';
+import { useModal } from './context/modal-context';
 
 function App() {
-  const [pokemonArray,setPokemonArray] = useState([])
+  const [pokemonArray, setPokemonArray] = useState([]); 
+  const [filteredPokemonArray, setFilteredPokemonArray] = useState([]); 
   const {searchValue} = useModal()
-
-  console.log(searchValue)
 
   useEffect(() => {
     const fetchData = async () => {
-        try {
-            const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
-            console.log(response.data);
-            setPokemonArray(response.data?.results)
-        } catch (error) {
-            console.error("Error fetching data:", error);
-        }
+      try {
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
+        setPokemonArray(response.data?.results);
+        setFilteredPokemonArray(response.data?.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
-    console.log(pokemonArray)
+
     fetchData();
-}, []);
+  }, []);
+
+  useEffect(() => {
+    
+    if (searchValue === '') {
+      setFilteredPokemonArray(pokemonArray);
+    } else {
+
+      const filtered = pokemonArray.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setFilteredPokemonArray(filtered);
+    }
+  }, [searchValue, pokemonArray]);
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className='main'>
-      {/* <Card data={{name:'bulbasor',url:"https://pokeapi.co/api/v2/pokemon/1/"}}/> */}
-
-      {
-        pokemonArray.map((pokemon,index)=>(
-          <Card key={index} data={{url:pokemon.url}}/>
-        ))
-      }
+        {filteredPokemonArray.map((pokemon, index) => (
+          <Card key={index} data={{ url: pokemon.url }} />
+        ))}
       </div>
-     
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
