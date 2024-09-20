6dabcac07621ed6f01e8887a5e340f66167451c8
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-09-20 15:34:37 +0530

    card component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Card from './components/Card/Card'
+import axios from 'axios'
 
 function App() {
 
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
+            console.log(response);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    fetchData();
+}, []);
 
   return (
     <>
       <Navbar/>
-      <Card/>
+      <div className='main'>
+      <Card data={{name:'bulbasor',url:"https://pokeapi.co/api/v2/pokemon/1/"}}/>
+      </div>
+     
+      
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,37 @@
-const Card = () =>{
+import { useEffect,useState } from "react"
+import axios from "axios";
+const Card = ({data}) =>{
+    const [pokemon,setPokemon] = useState(null)
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(data.url);
+                console.log(response.data)
+                setPokemon(response.data)
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+    
+        fetchData();
+    }, []);
+
+
     return(
         <>
-            <div className="card">
-                
+            <div className="card shadow">
+                <div className="card-image-container">
+                    <img className="card-image" src={pokemon?.sprites.front_default}/>
+
+                </div>
+                <div className="movie-details">
+                    <p className="title"> {pokemon?.name}</p>
+                    {
+                        pokemon?.types?.map((typeObj,index)=>(
+                            <p key={index} className="genre">{typeObj.type.name}</p>
+                        ))
+                    }
+                </div>
             </div>
         </>
     )
